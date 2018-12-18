58766175c51db683c1ea9570bf3fe7dd71716925
Krzysztof Kołodenny <dzidolos@gmail.com>
2018-12-18 16:28:42 +0100

    Tworzenie użytkowników ze skryptu

--- tworzenie.bat
+++ tworzenie.bat
@@ -1,14 +1,14 @@
 @echo off
 if "%1" == "" (goto wiadomosc)
 if not exist %1 (goto wiadomosc2)
-FOR /F "tokens=2,3 delims=." %%i in (%1) do @NET USER "%%i" %%j /add>nul
-FOR /F "tokens=2,3 delims=." %%i in (lista.txt) do ECHO Tworzony jest u¿ytkownik o nazwie %%i
+FOR /F "tokens=2,3 delims=." %%i in (%1) do @NET USERS "%%i" %%j /add>nul
+FOR /F "tokens=2,3 delims=." %%i in (lista.txt) do ECHO Tworzony jest uzytkownik %%i
 goto koniec
 :wiadomosc
-ECHO Nie podano parametru!
+echo Brak parametru - podaj nazwe pliku zawierajacego liste uzytkownikow
 goto koniec
 :wiadomosc2
-ECHO Podany parametr jest bledny!
+echo Podany parametr wejsciowy jest bledny!
 goto koniec
 :koniec
-pause 
+pause
